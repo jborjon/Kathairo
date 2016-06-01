4c941929cbea8ff97c2c59a996367ea7f8569706
Felipe Chora <fmchora@gmail.com>
2016-06-01 17:14:31 -0600

    hi!!!

--- app/src/main/java/edu/byui/theawesomes/kathairo2/MainActivity.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //hola
+    //hola cara bola
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
