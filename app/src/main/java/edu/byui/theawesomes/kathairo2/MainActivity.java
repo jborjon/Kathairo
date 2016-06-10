b44654a698057267d4ad650ddeb56b3b320a3be2
Felipe Chora <fmchora@gmail.com>
2016-06-09 18:52:58 -0600

    These are my two test Methods "Felipe"

--- app/src/main/java/edu/byui/theawesomes/kathairo2/MainActivity.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/MainActivity.java
@@ -6,12 +6,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //hola cara bola
-
+    private String userName;
+    private int number = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public int getNumber(){
+        return this.number;
+    }
+
+    public  boolean setUserName(String userName){
+        this.userName = userName;
+        if(this.userName != null)
+        return true;
+        else return false;
+    }
 
 }
