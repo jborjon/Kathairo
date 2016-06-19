aefb85921a4ac9411b4e91b0643821f7b33e7693
williamthe5thc <williamthe5thc@icloud.com>
2016-06-18 19:25:16 -0600

    Add files via upload
    here is some of the files hope it works!!!

---
+++ app/src/androidTest/java/edu/byui/theawesomes/kathairo2/ClueSolver.java
@@ -0,0 +1,28 @@
+package edu.byui.theawesomes.kathairo2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by Jordan on 6/8/16.
+ *
+ */
+public class ClueSolver extends ApplicationTestCase<Application> {
+
+
+    private String name;
+    //private timeCompleted Date;
+
+    public ClueSolver(){
+        super(Application.class);
+    }
+   public void getTimeCompleted() {
+
+   }
+    public void Operation1() {
+
+    }
+    /*public void toString() {
+
+    }*/
+}
---
+++ app/src/androidTest/java/edu/byui/theawesomes/kathairo2/CrosswordFrame.java
@@ -0,0 +1,67 @@
+package edu.byui.theawesomes.kathairo2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by Jordan on 6/8/16.
+ *
+ */
+public class CrosswordFrame extends ApplicationTestCase<Application> {
+
+    public CrosswordFrame(){
+        super(Application.class);
+        //do stuff
+
+    }
+    /*
+    private Gridpanel gridPanel;
+    private JTextField nameField;
+    public JPanel [] panel;
+    private jCheckbox solvedClueCheckBox;
+    public JButton saveButton;
+    public JButton loadButton;
+    public JScrollPane solvedLogPane;
+    public JScrollPane downListPane; 
+    public JScrollPane acrossListPane; 
+    public SolvedLog solvedLog;
+    public JList downList;
+    public JList acrossList;
+    public JLabel titleLabel;
+    public JLabel nameLabel;
+    public JLabel acrossLabel;
+    public JLabel downLabel;
+    public JLabel solvedLabel;
+    */
+
+    public void init() {
+
+    }
+    public void initGrid() {
+
+    }
+    public void createLists() {
+
+    }
+    public void createLabels() {
+
+    }
+    public void createSolvedLog() {
+
+    }
+    public void createButtons() {
+
+    }
+    public void createCheckBoxes() {
+
+    }
+    public void createTextFields() {
+
+    }
+    public void createGrid() {
+
+    }
+    public void createPanels() {
+
+    }
+}
