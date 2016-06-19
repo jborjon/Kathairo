c11727ef9525992cf52745ab87718f72b3378473
Jordan Charles <williamthe5thc@icloud.com>
2016-06-19 00:00:54 -0600

    Everybody's code

--- app/src/main/java/edu/byui/theawesomes/kathairo2/Crossword.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/Crossword.java
@@ -1,39 +1,49 @@
 package edu.byui.theawesomes.kathairo2;
 
+import java.util.ArrayList;
+
 /**
  * Created by Joseph on 6/18/16.
  */
 public class Crossword {
     public ArrayList<Clue> acrossClues;
+    public ArrayList<Clue> downClues;
     public String title;
     public int size;
     public String userName;
 
     public int getSize() {
         // TODO: implement
+        return size;
     }
 
     public String getTitle() {
         // TODO: implement
+        return  title;
     }
 
     public ArrayList<Clue> getAcrossClues() {
         // TODO: implement
+        return acrossClues;
     }
 
     public ArrayList<Clue> getDownClues() {
         // TODO: implement
+        return downClues;
     }
 
     public ArrayList<Clue> getClues() {
         // TODO: implement
+        return downClues;
     }
 
     public void setUserName(String userName) {
         // TODO: implement
+        this.userName = userName;
     }
 
     public String getUserName() {
         // TODO: implement
+        return  userName;
     }
 }
