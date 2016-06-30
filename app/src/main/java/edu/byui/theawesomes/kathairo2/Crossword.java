6bfaf910018da1d75b81760b91068d19766ccc63
Felipe Chora <fmchora@gmail.com>
2016-06-29 18:00:44 -0600

    Everybody's code

--- app/src/main/java/edu/byui/theawesomes/kathairo2/Crossword.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/Crossword.java
@@ -1,49 +1,23 @@
 package edu.byui.theawesomes.kathairo2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Joseph on 6/18/16.
+ * Created by fmcho on 6/29/2016.
  */
-public class Crossword {
-    public ArrayList<Clue> acrossClues;
-    public ArrayList<Clue> downClues;
-    public String title;
-    public int size;
-    public String userName;
+public class CrossWord {
+    private List<Word> crossword = new ArrayList<Word>();
+    /** we will have to create a function that reads an XML file and then to create words and clues objects.
+     */
+    public void addWord(String wordToSet, int numberOfWord){
+        Word toAdd = new Word();
+        toAdd.setWord(wordToSet);
+        toAdd.setNumberOfTheWord(numberOfWord);
+        crossword.add(toAdd);
 
-    public int getSize() {
-        // TODO: implement
-        return size;
     }
-
-    public String getTitle() {
-        // TODO: implement
-        return  title;
-    }
-
-    public ArrayList<Clue> getAcrossClues() {
-        // TODO: implement
-        return acrossClues;
-    }
-
-    public ArrayList<Clue> getDownClues() {
-        // TODO: implement
-        return downClues;
-    }
-
-    public ArrayList<Clue> getClues() {
-        // TODO: implement
-        return downClues;
-    }
-
-    public void setUserName(String userName) {
-        // TODO: implement
-        this.userName = userName;
-    }
-
-    public String getUserName() {
-        // TODO: implement
-        return  userName;
+    public List<Word> getCrosswordList(){
+        return crossword;
     }
 }
