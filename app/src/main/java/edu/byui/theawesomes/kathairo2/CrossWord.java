4b6db32767857bbdd3cd9c74ae8261a5bfd8a1d5
Joseph <bor12006@byui.edu>
2016-06-29 18:33:57 -0600

    Added Javadocs comment to addWord() in class CrossWord.

--- app/src/main/java/edu/byui/theawesomes/kathairo2/CrossWord.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/CrossWord.java
@@ -7,14 +7,21 @@ import java.util.List;
  * Created by fmcho on 6/29/2016.
  */
 public class CrossWord {
-    /*
+    /**
 
      */
     private List<Word> crossword = new ArrayList<Word>();
 
     /****************************************************
+     * Adds a word to the Crossword.
      *
+     * This method adds a word to the crossword puzzle. It
+     * takes a word and a number that is used to place the
+     * word properly.
      *
+     * @param wordToSet String The word to add
+     * @param numberOfWord int The position of the word
+     * @return Um...nothing. That's what 'void' means.
      *****************************************************/
     public void addWord(String wordToSet, int numberOfWord){
         Word toAdd = new Word();
