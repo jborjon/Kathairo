9063c0643570ffcdcdb33b7c62379bb50ee93fb3
Joseph <bor12006@byui.edu>
2016-07-02 17:02:18 -0600

    Added some bogus display stuff to the Clues activity.

--- app/src/main/java/edu/byui/theawesomes/kathairo2/cluesActivity.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/cluesActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class cluesActivity extends AppCompatActivity {
 
@@ -25,6 +28,31 @@ public class cluesActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Display the clues
+        displayClues();
     }
 
+
+    protected void displayClues() {
+
+        // Bogus data!
+        ArrayList<String> bogusData = new ArrayList<String>();
+
+        // Get the appropriate text field
+        TextView cluesTextField = (TextView) findViewById(R.id.cluesList);
+
+        // DELETE the following, it's just for testing
+        bogusData.add("My");
+        bogusData.add("name");
+        bogusData.add("is");
+        bogusData.add("Inigo");
+        bogusData.add("Montoya");
+
+        // Put the clues on the screen
+        for (int i = 0; i < bogusData.size(); ++i) {
+            // Add a list number, starting with 1, followed by the clue
+            cluesTextField.append(Integer.toString(i + 1) + ". " + bogusData.get(i) + "\n");
+        }
+    }
 }
