0d79f80b6846a180701959b4fb4591e2f03f65e1
Jordan Charles <williamthe5thc@icloud.com>
2016-07-15 00:50:10 -0600

    added //random comment to get this commited and pushed! to update everythign for sure on github

--- app/src/main/java/edu/byui/theawesomes/kathairo2/AboutActivity.java
+++ app/src/main/java/edu/byui/theawesomes/kathairo2/AboutActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 //random comment to get this commited and pushed!
+//random comment to get this commited and pushed!
 
 public class AboutActivity extends AppCompatActivity {
 
